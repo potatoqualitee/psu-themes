3d039b8acc7006907babff18d22c06a0c789a933
Chrissy LeMaire <clemaire@gmail.com>
2022-08-23 15:35:07 +0200

    add function

--- Repository/dashboards/kbupdate/private/functions.ps1
+++ Repository/dashboards/kbupdate/private/functions.ps1
@@ -25,3 +25,112 @@ function New-NavSwitch ($nav, $item, [switch]$disabled) {
 function Get-Colors {
     Get-Content -Path (Join-Path -Path $script:UDRoot -ChildPath colors.json) | ConvertFrom-Json
 }
+
+function Test-DarkColor {
+    <#
+    .SYNOPSIS
+    Tests an rgb or hex value to see if it's considered light or dark
+
+    .PARAMETER Color
+    The color to test in Hex or RGB
+    
+    .NOTES
+    Thanks to:
+    https://github.com/EvotecIT/PSSharedGoods/blob/master/Public/Converts/Convert-Color.ps1
+    https://awik.io/determine-color-bright-dark-using-javascript/
+
+    .EXAMPLE
+    Test-DarkColor ffffff
+    False
+    
+    .EXAMPLE
+    Test-DarkColor 000000
+    True
+
+    .EXAMPLE
+    Test-DarkColor "rgb(255,255,255)"
+    False
+
+    .EXAMPLE
+    Test-DarkColor "rgb(255,255,255)"
+    VERBOSE: 255
+    VERBOSE: 255
+    VERBOSE: 255
+    VERBOSE: 255
+    False
+   
+    #>
+    [cmdletbinding()]
+    param(
+        [string[]]$Color
+    )
+    # Clean up
+    if ($first = $Color[1]) {
+        $cleanedcolor = $Color -join ","
+        if ($first -notmatch "rgb" -and $first -notmatch "\(") {
+            $cleanedcolor = "rgb($cleanedcolor)"
+        }
+    } else {
+        $cleanedcolor = "$Color"
+    }
+    $cleanedcolor = $cleanedcolor.Replace('#','')
+    $cleanedcolor = $cleanedcolor.Replace(' ','')
+
+    if ($cleanedcolor -match '^rgb') {
+        try {
+            # If RGB --> store the red, green, blue values in separate variables
+            $rgb = $cleanedcolor -match '^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)$'
+            if (-not $rgb) {
+                $rgb = $cleanedcolor -match '^([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])$'
+            }
+            
+            $r = [convert]::ToInt32($matches[1])
+            $g = [convert]::ToInt32($matches[2])
+            $b = [convert]::ToInt32($matches[3])
+        } catch {
+            Write-Warning "$cleanedcolor isnt a valid rgb color for the purposes of the script: $PSItem"
+            return
+        }
+    } else {
+        
+        try {
+            $null = [int]::Parse($cleanedcolor,[System.Globalization.NumberStyles]::HexNumber)
+            if ($cleanedcolor.Length -eq 3) {
+                # this could be done with pure regex but alas
+                # if someone passed in a shortcut html, expand it
+                $temp = @()
+                foreach ($char in $cleanedcolor.ToCharArray()) {
+                    $temp += $char, $char
+                }
+                $cleanedcolor = $temp -join ""
+            }
+            $r = $cleanedcolor.Remove(2, 4)
+            $g = $cleanedcolor.Remove(4, 2)
+            $g = $g.remove(0, 2)
+            $b = $cleanedcolor.Remove(0, 4)
+            $r = [convert]::ToInt32($r, 16)
+            $g = [convert]::ToInt32($g, 16)
+            $b = [convert]::ToInt32($b, 16)
+        } catch {
+            Write-Warning "$cleanedcolor is not a valid hex for the purposes of the script"
+            return
+        }
+    }
+    
+    $r, $g, $b | Write-Verbose
+    # HSP (Highly Sensitive Ppl) equation from http:#alienryderflex.com/hsp.html
+    $hsp = [math]::Sqrt(
+        0.299 * ($r * $r) +
+        0.587 * ($g * $g) +
+        0.114 * ($b * $b)
+    )
+
+    # Using the HSP value, determine whether the $cleanedcolor is light or dark
+    if ($hsp -gt 127.5) {
+        $hsp | Write-Verbose
+        return $false
+    } else {
+        $hsp | Write-Verbose
+        return $true
+    }
+}
