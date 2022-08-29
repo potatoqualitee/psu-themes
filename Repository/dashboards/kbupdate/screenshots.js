cde5e6d66e76e828a76fce4b55da0c7140110526
Chrissy LeMaire <clemaire@gmail.com>
2022-08-29 17:38:40 +0200

    path updates

--- Repository/dashboards/kbupdate/screenshots.js
+++ Repository/dashboards/kbupdate/screenshots.js
@@ -17,7 +17,11 @@ var { chromium } = require('playwright');
         [file].forEach(async function (themefile) {
             // launch chromium browser
             var browser = await chromium.launch({
-                headless: true
+                headless: false,
+                viewport: {
+                    width: 1280, // 1280 in commit 1ee6578
+                    height: 1720, // 720 in commit 1ee6578
+                }
             });
             // create viewport with dark mode enabled,
             var context = await browser.newContext({});
@@ -27,16 +31,16 @@ var { chromium } = require('playwright');
             // connect to webpage
             var page = await context.newPage();
             await page.goto('http://localhost:5000');
-            //setTimeout(() => console.log("waiting"), 3000);
+            // await scrollFullPage(page);
             // wait for the page to load
             await page.waitForSelector('text=Table with Paging');
             // take a screenshot
-            var lightscreenshot = path.join(__dirname, `${theme}-light.png`);
+            var lightscreenshot = path.join(__dirname, 'themes', 'screenshots', `${theme}-light.png`);
             await page.screenshot({ path: lightscreenshot, fullPage: true });
             // click the dark mode toggle
             await page.locator('header button').click();
             // take a screenshot
-            var darkscreenshot = path.join(__dirname, `${theme}-dark.png`);
+            var darkscreenshot = path.join(__dirname, 'themes', 'screenshots', `${theme}-dark.png`);
             await page.screenshot({ path: darkscreenshot, fullPage: true });
            
             await context.close();
@@ -44,3 +48,22 @@ var { chromium } = require('playwright');
         });
     });
 })();
+
+async function scrollFullPage(page) {
+    await page.evaluate(async () => {
+        await new Promise(resolve => {
+            let totalHeight = 0;
+            const distance = 100;
+            const timer = setInterval(() => {
+                const scrollHeight = document.body.scrollHeight;
+                window.scrollBy(0, distance);
+                totalHeight += distance;
+
+                if (totalHeight >= scrollHeight) {
+                    clearInterval(timer);
+                    resolve();
+                }
+            }, 100);
+        });
+    });
+}
