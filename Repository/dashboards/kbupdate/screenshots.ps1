aa4115d48ca81ca858676ef8839d3400b21df06c
Chrissy LeMaire <clemaire@gmail.com>
2022-08-29 22:35:09 +0200

    Update screenshots.ps1

--- Repository/dashboards/kbupdate/screenshots.ps1
+++ Repository/dashboards/kbupdate/screenshots.ps1
@@ -6,5 +6,6 @@ foreach ($file in $files) {
     $theme = $file.BaseName
     Set-ActiveTheme -Theme $theme
     Start-Sleep 2
-    node $script:UDRoot\screenshots.js "$theme"
+    $screenshots = Join-Path -Path $script:UDRoot -ChildPath screenshots.js
+    node $screenshots "$theme"
 }
