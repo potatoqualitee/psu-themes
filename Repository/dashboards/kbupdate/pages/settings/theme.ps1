2ac4272bf971ebea01e5a2b1aa8afd3e51497816
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2022-09-01 21:27:27 +0200

    Contrast fixes (#4)

--- Repository/dashboards/kbupdate/pages/settings/theme.ps1
+++ Repository/dashboards/kbupdate/pages/settings/theme.ps1
@@ -1,14 +1,5 @@
 New-UDPage -Name Theme -Url /settings/theme -Content {
-    $allthemes = Get-AllThemes
     $activetheme = Get-ActiveTheme
-    
-    foreach ($theme in $allthemes) {
-        if ($activetheme -eq "$theme") {
-            $checked = $true
-        } else {
-            $checked = $false
-        }
-    }
     
     $featured = "1984", "Blue Matrix", "Broadcast", "Chalk", "ChallengerDeep", "Chester", "Cyber Cube", "Dark+", "Darkside", "Dracula", "Duckbones", "Elementary", "Espresso", "FirefoxDev", "Firewatch", "Flat", "Galaxy", "Galizur", "Glorious", "Grape", "h4rithd.com", "Gruvbox", "Highway", "Hopscotch.256", "iceberg", "idletoes", "JetBrains Dracula", "Jubi", "Later This Evening", "Liquid Carbon", "lovelace", "Material", "MonaLisa", "Obsidian", "Oceanic-Next", "One", "Pencil", "potatoqualitee", "Pro", "Overnight Slumber", "PaleNightHC", "Pandodra", "Paraiso", "PaulMillr", "Pencil", "Retrowave", "Ryuuko", "Sakura", "Scarlet Protocol", "seoulbones", "Serendipity Sunset", "SleepyHollow", "Sublette", "Subliminal", "synthwave-everything", "Tinacious Design", "Tokyo", "Tomorrow Night Eighties", "Tomorrow", "Treehouse", "Twi", "Ubuntu", "Ultra", "UnderTheSea", "Urple", "Vimbones", "Violet", "Wez", "wilmersdorf", "zenbones"
 
@@ -25,22 +16,23 @@ New-UDPage -Name Theme -Url /settings/theme -Content {
                             Set-ActiveTheme ((Get-UDElement -Id $PSItem)).attributes.id
                         }
                     } -Content {
-                        New-UDCard -TitleAlignment Center -Title $PSItem -Content {
-                            New-UDImage -Url /assets/screenshots/$PSItem-light.png -Attributes @{
-                                style = @{
-                                    width = "75%"
-                                }
-                            }
+                        if ($PSItem -eq $ActiveTheme) {
+                            $style = @{ "background-color" = "#BBBBC4" }
+                            $title = "$PSItem (Active)"
+                        } else {
+                            $title = $PSItem
+                        }
+                        New-UDCard -Style $style -TitleAlignment Center -Title $title -Content {
+                            New-UDElement -Tag "center" -Content {
+                                New-UDImageZoom -Path /assets/screenshots/$PSItem-light.png -Zoom 3 -Delay 0 -Width 387 -Height 200
                             
-                            New-UDElement -Tag "div" -Attributes @{
-                                style = @{
-                                    "padding" = "10px"
+                                New-UDElement -Tag "div" -Attributes @{
+                                    style = @{
+                                        "padding" = "10px"
+                                    }
                                 }
-                            }
-                            New-UDImage -Url /assets/screenshots/$PSItem-dark.png -Attributes @{
-                                style = @{
-                                    width = "75%"
-                                }
+                            
+                                New-UDImageZoom -Path /assets/screenshots/$PSItem-dark.png -Zoom 3 -Delay 0 -Width 387 -Height 200
                             }
                         }
                     } -Id $PSItem
@@ -60,22 +52,23 @@ New-UDPage -Name Theme -Url /settings/theme -Content {
                             Set-ActiveTheme ((Get-UDElement -Id $PSItem)).attributes.id
                         }
                     } -Content {
-                        New-UDCard -TitleAlignment Center -Title $PSItem -Content {
-                            New-UDImage -Url /assets/screenshots/$PSItem-light.png -Attributes @{
-                                style = @{
-                                    width = "75%"
-                                }
-                            }
+                        if ($PSItem -eq $ActiveTheme) {
+                            $style = @{ "background-color" = "#BBBBC4" }
+                            $title = "$PSItem (Active)"
+                        } else {
+                            $title = $PSItem
+                        }
+                        New-UDCard -Style $style -TitleAlignment Center -Title $title -Content {
+                            New-UDElement -Tag "center" -Content {
+                                New-UDImageZoom -Path /assets/screenshots/$PSItem-light.png -Zoom 3 -Delay 0 -Width 387 -Height 200
                             
-                            New-UDElement -Tag "div" -Attributes @{
-                                style = @{
-                                    "padding" = "10px"
+                                New-UDElement -Tag "div" -Attributes @{
+                                    style = @{
+                                        "padding" = "10px"
+                                    }
                                 }
-                            }
-                            New-UDImage -Url /assets/screenshots/$PSItem-dark.png -Attributes @{
-                                style = @{
-                                    width = "75%"
-                                }
+                            
+                                New-UDImageZoom -Path /assets/screenshots/$PSItem-dark.png -Zoom 3 -Delay 0 -Width 387 -Height 200
                             }
                         }
                     } -Id $PSItem
--- Repository/dashboards/kbupdate/screenshots.js
+++ Repository/dashboards/kbupdate/screenshots.js
@@ -3,13 +3,13 @@ var { chromium } = require('playwright');
 
 (async function () {
     var themeparam = process.argv[2];
-    
+
     var path = require('path');
     var fs = require('fs');
     var directoryPath = path.join(__dirname, 'themes');
     var file = path.join(directoryPath, `${themeparam}.json`);
     console.log("processing " + file);
-   
+
     fs.readdir(directoryPath, async function (err) {
         if (err) {
             return console.log('Unable to scan directory: ' + err);
@@ -17,15 +17,19 @@ var { chromium } = require('playwright');
         [file].forEach(async function (themefile) {
             // launch chromium browser
             var browser = await chromium.launch({
-                headless: true,
-                viewport: {
-                    width: 1280, // 1280 in commit 1ee6578
-                    height: 720, // 720 in commit 1ee6578
-                }
+                headless: true
             });
             // creating viewport with dark mode enabled doesn't work on this site
             // just create an empty context
-            var context = await browser.newContext({});
+            //var context = await browser.newContext({});
+            // create new context with 1920×1080
+            var context = await browser.newContext({
+                viewport: {
+                    width: 1520,
+                    height: 790,
+                }
+            });
+
             // get basename of themefile
             var theme = path.basename(themefile, '.json');
             // connect to webpage
@@ -33,17 +37,19 @@ var { chromium } = require('playwright');
             await page.goto('http://localhost:5000');
             // wait for the page to load
             await page.waitForSelector('text=Table with Paging');
+            await page.waitForSelector('text=Table with Paging');
             // take a screenshot
             var lightscreenshot = path.join(__dirname, 'assets', 'screenshots', `${theme}-light.png`);
-            await page.screenshot({ path: lightscreenshot, fullPage: true });
+            await page.screenshot({ path: lightscreenshot });
             // click the dark mode toggle
             await page.locator('header button').click();
             // wait for the page to load
             await page.waitForSelector('text=Table with Paging');
+            await page.waitForSelector('text=Table with Paging');
             // take a screenshot
             var darkscreenshot = path.join(__dirname, 'assets', 'screenshots', `${theme}-dark.png`);
-            await page.screenshot({ path: darkscreenshot, fullPage: true });
-           
+            await page.screenshot({ path: darkscreenshot });
+
             await context.close();
             await browser.close();
         });
