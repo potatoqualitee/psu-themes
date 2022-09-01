f41c7ec9229d8b9e514046175778643ceeb39243
Chrissy LeMaire <clemaire@gmail.com>
2022-09-01 08:52:03 +0200

    Update screenshots.ps1

--- Repository/dashboards/kbupdate/screenshots.ps1
+++ Repository/dashboards/kbupdate/screenshots.ps1
@@ -1,14 +1,12 @@
 $global:UDRoot = $script:UDRoot = $PSScriptRoot
 . "$PSScriptRoot\private\functions.ps1"
-$files = Get-ChildItem "$PSScriptRoot\themes\*json" | Select-Object -First 10
+$files = Get-ChildItem "$PSScriptRoot\themes\*json" #| Select-Object -First 10
 
 foreach ($file in $files) {
     $theme = $file.BaseName
     Set-ActiveTheme -Theme $theme
     if ($Env:CI) {
-        Write-Warning "CI environment detected"
         $colorfile = Join-Path -Path $script:UDRoot -ChildPath colors.json
-        Write-Warning $colorfile
         docker cp $colorfile powershelluniversal:/root/.PowerShellUniversal/Repository/dashboards/kbupdate/colors.json
     }
     Start-Sleep 2
