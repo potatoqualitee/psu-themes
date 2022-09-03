bd77a54a3f52343036f081a58aa275a0d2f8b93d
Chrissy LeMaire <clemaire@gmail.com>
2022-09-03 05:14:50 +0200

    Update integration.test.js

--- tests/integration.test.js
+++ tests/integration.test.js
@@ -4,7 +4,7 @@ import { expect } from '@playwright/test';
 test.describe('Output Edit', () => {
   test('Simulations page', async ({ page }) => {
       await page.goto('http://localhost:5000/');
-      await page.waitForSelector('text=Table with Paging');
+      page.waitForSelector('text=Table with Paging');
 
       // Click div[role="button"]:has-text("Elements")
       page.locator('div[role="button"]:has-text("Elements")').click();
