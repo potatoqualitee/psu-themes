790f84e2c797c508d88d6ef5ef2c64831942c210
Chrissy LeMaire <clemaire@gmail.com>
2022-09-03 05:22:42 +0200

    upd

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,13 +1,7 @@
-import it, { expect } from '@playwright/test'
+import it, { expect } from '@playwright/test';
 
 it('is a basic test with the page', async ({ page }) => {
-    await page.goto('http://localhost:5000/');
-
-    // Click div[role="button"]:has-text("Elements")
-    await page.locator('div[role="button"]:has-text("Elements")').click();
-    await expect(page).toHaveURL('http://localhost:5000/elements');
-
-    // Click div[role="button"]:has-text("Select Theme")
-    await page.locator('div[role="button"]:has-text("Select Theme")').click();
-    await expect(page).toHaveURL('http://localhost:5000/theme');
+    await page.goto('https://playwright.dev/');
+    const name = await page.innerText('.home-navigation');
+    expect(name).toBe('🎭 Playwright');
 });
