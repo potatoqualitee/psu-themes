7e4b05620402492e67265e633e3ff4e923c44886
Chrissy LeMaire <clemaire@gmail.com>
2022-09-03 05:08:29 +0200

    Update integration.test.js

--- tests/integration.test.js
+++ tests/integration.test.js
@@ -1,16 +1,17 @@
 const { test } = require('@playwright/test');
+import { expect } from '@playwright/test';
 
 test.describe('Output Edit', () => {
-  test('Simulations page', function ({ page }) {
-          page.goto('http://localhost:5000/');
-          page.waitForSelector('text=Table with Paging');
+  test('Simulations page', async ({ page }) => {
+      page.goto('http://localhost:5000/');
+      page.waitForSelector('text=Table with Paging');
 
-          // Click div[role="button"]:has-text("Elements")
-          page.locator('div[role="button"]:has-text("Elements")').click();
-          expect(page).toHaveURL('http://localhost:5000/elements');
+      // Click div[role="button"]:has-text("Elements")
+      page.locator('div[role="button"]:has-text("Elements")').click();
+      await expect(page).toHaveURL('http://localhost:5000/elements');
 
-          // Click div[role="button"]:has-text("Select Theme")
-          page.locator('div[role="button"]:has-text("Select Theme")').click();
-          expect(page).toHaveURL('http://localhost:5000/theme');
-      });
+      // Click div[role="button"]:has-text("Select Theme")
+      page.locator('div[role="button"]:has-text("Select Theme")').click();
+      await expect(page).toHaveURL('http://localhost:5000/theme');
+  });
 })
