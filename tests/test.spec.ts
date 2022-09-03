3c06f13d3ef63ba057e0d837e3f3597ca2463d6c
Chrissy LeMaire <clemaire@gmail.com>
2022-09-03 12:20:43 +0200

    Update test.spec.ts

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test('homepage has PowerShell in title and get started link linking to the intro page', async ({ page }) => {
   await page.goto('http://localhost:5000/');
 
-  // Expect a title "to contain" a substring.
+  // this is a minimal test -- i gotta figure out how to do more later
   await expect(page).toHaveTitle(/PowerShell Universal Dashboard/);
   await expect(page).toHaveURL('http://localhost:5000/');
 });
