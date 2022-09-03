90f6ba561d6eb00174516fbcb2dbdcf94fe96ca2
Chrissy LeMaire <clemaire@gmail.com>
2022-09-03 10:35:16 +0200

    FINE

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,20 +1,14 @@
 import { test, expect } from '@playwright/test';
 
 test('homepage has Playwright in title and get started link linking to the intro page', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('http://localhost:5000/');
+  page.waitForSelector('text=Table with Paging');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  // Click div[role="button"]:has-text("Elements")
+  page.locator('div[role="button"]:has-text("Elements")').click();
+  await expect(page).toHaveURL('http://localhost:5000/elements');
 
-  // create a locator
-  const getStarted = page.locator('text=Get Started');
-
-  // Expect an attribute "to be strictly equal" to the value.
-  await expect(getStarted).toHaveAttribute('href', '/docs/intro');
-
-  // Click the get started link.
-  await getStarted.click();
-
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
+  // Click div[role="button"]:has-text("Select Theme")
+  page.locator('div[role="button"]:has-text("Select Theme")').click();
+  await expect(page).toHaveURL('http://localhost:5000/theme');
 });
