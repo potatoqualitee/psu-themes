38bbd08b72569d7a05d191ea8f023091a1087ba7
Chrissy LeMaire <potatoqualitee@users.noreply.github.com>
2022-09-03 12:45:19 +0200

    Readme update and minimal cleanup (#6)

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,27 +1,9 @@
-const playwright = require('playwright');
-const { test } = require('@playwright/test');
-import { expect } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-test('Simulations page', async ({ page }) => {
-    for (const browserType of ['chromium', 'firefox', 'webkit']) {
-        const browser = await playwright[browserType].launch();
-        try {
-            await page.goto('http://localhost:5000/');
-            page.waitForSelector('text=Table with Paging');
+test('homepage has PowerShell in title and get started link linking to the intro page', async ({ page }) => {
+  await page.goto('http://localhost:5000/');
 
-            // Click div[role="button"]:has-text("Elements")
-            page.locator('div[role="button"]:has-text("Elements")').click();
-            await expect(page).toHaveURL('http://localhost:5000/elements');
-
-            // Click div[role="button"]:has-text("Select Theme")
-            page.locator('div[role="button"]:has-text("Select Theme")').click();
-            await expect(page).toHaveURL('http://localhost:5000/theme');
-        } catch (ex) {
-            var png = "/tmp/screenshots/" + browserType + ".png";
-            await page.screenshot({ path: png });
-            throw ex
-        } finally {
-            await browser.close();
-        }
-    }
-})();
+  // this is a minimal test -- i gotta figure out how to do more later
+  await expect(page).toHaveTitle(/PowerShell Universal Dashboard/);
+  await expect(page).toHaveURL('http://localhost:5000/');
+});
