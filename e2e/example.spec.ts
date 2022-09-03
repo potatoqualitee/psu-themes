a402654c0106c66baf29346d3521bd81cbab5551
Chrissy LeMaire <clemaire@gmail.com>
2022-09-03 10:32:19 +0200

    Update example.spec.ts

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,27 +1,20 @@
-const playwright = require('playwright');
-const { test } = require('@playwright/test');
-import { expect } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-test('Simulations page', async ({ page }) => {
-  for (const browserType of ['chromium', 'firefox', 'webkit']) {
-    const browser = await playwright[browserType].launch();
-    try {
-      await page.goto('http://localhost:5000/');
-      page.waitForSelector('text=Table with Paging');
+test('homepage has Playwright in title and get started link linking to the intro page', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
 
-      // Click div[role="button"]:has-text("Elements")
-      page.locator('div[role="button"]:has-text("Elements")').click();
-      await expect(page).toHaveURL('http://localhost:5000/elements');
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
 
-      // Click div[role="button"]:has-text("Select Theme")
-      page.locator('div[role="button"]:has-text("Select Theme")').click();
-      await expect(page).toHaveURL('http://localhost:5000/theme');
-    } catch (ex) {
-      var png = "/tmp/screenshots/" + browserType + ".png";
-      await page.screenshot({ path: png });
-      throw ex
-    } finally {
-      await browser.close();
-    }
-  }
-})();
+  // create a locator
+  const getStarted = page.locator('text=Get Started');
+
+  // Expect an attribute "to be strictly equal" to the value.
+  await expect(getStarted).toHaveAttribute('href', '/docs/intro');
+
+  // Click the get started link.
+  await getStarted.click();
+
+  // Expects the URL to contain intro.
+  await expect(page).toHaveURL(/.*intro/);
+});
