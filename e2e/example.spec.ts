7b66be8d1b9fd4bba596ee21b8eb51dc904b9b2f
Chrissy LeMaire <clemaire@gmail.com>
2022-09-03 11:27:56 +0200

    Update example.spec.ts

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -5,15 +5,5 @@ test('homepage has PowerShell in title and get started link linking to the intro
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/PowerShell Universal Dashboard/);
-
   await expect(page).toHaveURL('http://localhost:5000/');
-
-  // Click div[role="button"]:has-text("Sidebar")
-  await page.locator('div[role="button"]:has-text("Sidebar")').click();
-  await expect(page).toHaveURL('http://localhost:5000/sidebar');
-  
-  // Uncheck #Home
-  await page.locator('#Home').uncheck();
-  await expect(page).toHaveURL('http://localhost:5000/sidebar');
-
 });
