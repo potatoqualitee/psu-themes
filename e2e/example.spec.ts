ea69f88c56a1e6d83c729ca39870108a193324d1
Chrissy LeMaire <clemaire@gmail.com>
2022-09-03 10:52:31 +0200

    Update example.spec.ts

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,30 +1,12 @@
 import { test, expect } from '@playwright/test';
 
 test('homepage has PowerShell in title and get started link linking to the intro page', async ({ page }) => {
-
   await page.goto('http://localhost:5000/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/PowerShell Universal Dashboard/);
 
-  // create a locator
-  const supportedIcons = page.locator('text=Supported Icons');
-
-  // Expect an attribute "to be strictly equal" to the value.
-  await expect(supportedIcons).toHaveAttribute('href', '/icons');
-
-  // Click the get started link.
-  await supportedIcons.click();
-
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*icons/);
-  
-  // Click div[role="button"]:has-text("Elements")
-  // page.locator('div[role="button"]:has-text("Elements")').click();
-  // await expect(page).toHaveURL('http://localhost:5000/elements');
-
   // Click div[role="button"]:has-text("Select Theme")
-  // page.locator('div[role="button"]:has-text("Select Theme")').click();
-  // await expect(page).toHaveURL('http://localhost:5000/theme');
-
+  page.locator('div[role="button"]:has-text("Select Theme")').click();
+  await expect(page).toHaveURL('http://localhost:5000/theme');
 });
